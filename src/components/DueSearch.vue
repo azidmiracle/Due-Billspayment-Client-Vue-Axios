9407ac4c262ecb2dc2a81963ec13dfba537a32d7
emsky7288 <emelydiazbusiness@gmail.com>
2020-06-13 12:47:36 +0800

    Search engine
    Search engine

--- src/components/DueSearch.vue
+++ src/components/DueSearch.vue
@@ -5,11 +5,11 @@
       </ion-col>
       <ion-col>
         <ion-item>
-          <ion-label>Zipcode:</ion-label>
+          <ion-label>Bills Name:</ion-label>
           <ion-input
             type="number"
-            :value="zip"
-            @input="zip = $event.target.value"
+            :value="bills_name"
+            @input="bills_name = $event.target.value"
             name="zip"
             placeholder="Type bills name here"
           ></ion-input>
@@ -25,9 +25,13 @@
 <script>
 export default {
   name: "DueSearch",
+  data(){
+      return{
+         bills_name:null,
+      }
+  },
   methods:{
-      OnSubmit(){
-          
+      OnSubmit(){       
       }
   }
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,32 @@
 <template>
   <div class="ion-page">
+    <ion-header translucent>
+      <ion-toolbar>
+        <ion-title>Search Bill's Name</ion-title>
+      </ion-toolbar>
+      <ion-toolbar>
+        <ion-searchbar
+          :value="bills_name"
+          @input="bills_name = $event.target.value"
+          @ionBlur="searchDue(bills_name)"
+          show-cancel-button="always"
+          @ionCancel="loadOrigValue"
+        ></ion-searchbar>
+      </ion-toolbar>
+    </ion-header>
     <ion-content class="ion-padding">
-      <DueList :billsList="billsList" v-on:deleteList="onDeleteList" :username="username"   v-on:updateList="updateList"/>
-      <ion-fab vertical="bottom" horizontal="end" slot="fixed" @click="openAddListModal"> 
+      <DueList
+        :billsList="billsList"
+        v-on:deleteList="onDeleteList"
+        :username="username"
+        v-on:updateList="updateList"
+      />
+      <ion-fab
+        vertical="bottom"
+        horizontal="end"
+        slot="fixed"
+        @click="openAddListModal"
+      >
         <ion-fab-button color="secondary">
           <ion-icon name="add"></ion-icon>
         </ion-fab-button>
@@ -20,24 +44,24 @@ export default {
   name: "Home",
   components: {
     DueList,
-   // IonTabs
   },
   data() {
     return {
       billsList: null,
-      username:null,
+      billList_Orig: null, //save the lists of due for searching purposes
+      username: null,
+      billsList_filtered: [],
+      bills_name: null,
     };
-  }
-  ,
+  },
   mounted() {
     this.getAlldues();
-    this.username=this.$route.params.username;
-    
-  }
-  ,
+    this.username = this.$route.params.username;
+  },
   methods: {
     async getAlldues() {
       this.billsList = await Due.getAllDueLists(this.$route.params.username);
+      this.billList_Orig = this.billsList; //save the value to the billListoRIG for search purpose
     },
     async openAddListModal() {
       let modal = await this.$ionic.modalController.create({
@@ -45,9 +69,9 @@ export default {
         componentProps: {
           propsData: {
             timeStamp: new Date(),
-            username:this.username
-          }
-        }
+            username: this.username,
+          },
+        },
       });
 
       // show the modal
@@ -60,13 +84,28 @@ export default {
     onDeleteList(value) {
       let isDeleted = confirm("Do you want to delete?");
       if (isDeleted) {
-        Due.deleteDue(value).then(()=> this.getAlldues()); 
+        Due.deleteDue(value).then(() => this.getAlldues());
       }
       console.log(value);
     },
-    updateList(){
-      this.getAlldues()
-    }
-  }
+    updateList() {//this is called everytime the child component has changes
+      this.getAlldues();
+    },
+    loadOrigValue() {//use for search engine. this is not to avoid loading again the data from server side
+      this.billsList = this.billList_Orig; //put back the old value
+    },
+    searchDue(text) {
+      this.loadOrigValue();
+      this.billsList.forEach((element) => {
+        let regExSearch = new RegExp(text, "gi");
+        let obj = element.bills_name.search(regExSearch);
+        if (obj >= 0) {
+          this.billsList_filtered.push(element);
+        }
+      });
+      this.billsList = this.billsList_filtered;
+      this.billsList_filtered = []; //clear the filtered arrays
+    },
+  },
 };
 </script>
